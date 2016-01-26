b6d5670c9568ce654bce960aa372fa540e8b64a1
StenListra <sten.listra@gmail.com>
2016-01-26 16:20:29 +0000

    Adding video upload to server

---
+++ app/src/main/java/com/example/meelis/virtualdrivinginstructor/Constants.java
@@ -0,0 +1,8 @@
+package com.example.meelis.virtualdrivinginstructor;
+
+/**
+ * Created by SLIST on 26-Jan-16.
+ */
+public class Constants {
+    public static final String UPLOAD_URL = "http://140.203.252.115:3000/upload";
+}
--- app/src/main/java/com/example/meelis/virtualdrivinginstructor/LessonEndActivity.java
+++ app/src/main/java/com/example/meelis/virtualdrivinginstructor/LessonEndActivity.java
@@ -1,19 +1,40 @@
 package com.example.meelis.virtualdrivinginstructor;
 
+import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.content.DialogInterface;
 import android.location.Location;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import java.io.File;
 import java.util.ArrayList;
 
-public class LessonEndActivity extends AppCompatActivity{
+public class LessonEndActivity extends AppCompatActivity {
 
     public static FragmentManager mFragmentManager;
     private long mDuration;
     private LessonMapFragment mMapFragment;
-    private TextView mDistanceTextView, mDurationTextView;
+    private TextView mDistanceTextView, mDurationTextView, mTxtPercentage;
+    private ProgressBar mProgressBar;
+    private String mFilePath = Environment.getExternalStorageDirectory() + "/video.mp4";
+    private Button mBtnUpload;
+    long mTotalSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +43,17 @@ public class LessonEndActivity extends AppCompatActivity{
 
         mDistanceTextView = (TextView) findViewById(R.id.distanceTextView);
         mDurationTextView = (TextView) findViewById(R.id.durationTextView);
+        mTxtPercentage = (TextView) findViewById(R.id.percentageView);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mBtnUpload = (Button) findViewById(R.id.uploadButton);
+
+        mBtnUpload.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View w) {
+                new FileUploader().execute();
+            }
+        });
 
         mFragmentManager = getFragmentManager();
         mMapFragment = (LessonMapFragment) mFragmentManager.findFragmentById(R.id.mapFragment);
@@ -32,15 +64,109 @@ public class LessonEndActivity extends AppCompatActivity{
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         String distance = mMapFragment.getDistance();
-        if (distance != null){
+        if (distance != null) {
             mDistanceTextView.setText(String.format(getResources().getString(R.string.distance_0_00_km), distance));
-        }
-        else{
+        } else {
             mDistanceTextView.setText(String.format(getResources().getString(R.string.distance_0_00_km), "0 m"));
         }
-        mDurationTextView.setText(String.format(getResources().getString(R.string.duration_s), mDuration/60000));
+        mDurationTextView.setText(String.format(getResources().getString(R.string.duration_s), mDuration / 60000));
+    }
+
+    private class FileUploader extends AsyncTask<Void, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            // setting progress bar to zero
+            mProgressBar.setProgress(0);
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            // Making progress bar visible
+            mProgressBar.setVisibility(View.VISIBLE);
+
+            // updating progress bar value
+            mProgressBar.setProgress(progress[0]);
+
+            // updating percentage value
+            mTxtPercentage.setText(String.valueOf(progress[0]) + "%");
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return uploadFile();
+        }
+
+        @SuppressWarnings("deprecation")
+        private String uploadFile() {
+            String responseString;
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(Constants.UPLOAD_URL);
+
+            try {
+                ProgressMultiPartEntity entity = new ProgressMultiPartEntity(
+                        new ProgressMultiPartEntity.ProgressListener() {
+
+                            @Override
+                            public void transferred(long num) {
+                                publishProgress((int) ((num / (float) mTotalSize) * 100));
+                            }
+                        });
+
+                File sourceFile = new File(mFilePath);
+
+                // Adding file data to http body
+                entity.addPart("video", new FileBody(sourceFile));
+
+                mTotalSize = entity.getContentLength();
+                httppost.setEntity(entity);
+
+                // Making server call
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity r_entity = response.getEntity();
+
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    // Server response
+                    responseString = EntityUtils.toString(r_entity);
+                } else {
+                    responseString = "Error occurred! Http Status Code: "
+                            + statusCode;
+                }
+
+            } catch (Exception e) {
+                responseString = e.toString();
+            }
+
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.e("LessonEndActivity", "Response from server: " + result);
+
+            // showing the server response in an alert dialog
+            showAlert(result);
+
+            super.onPostExecute(result);
+        }
+    }
+
+    private void showAlert(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message).setTitle("Response from Server")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // do nothing
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
